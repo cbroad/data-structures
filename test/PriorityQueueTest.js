5781b503918b00023bb86a1a71b31e91877cfc9f
Carl Broadbent <carl.broadbent@gmail.com>
2025-02-10 23:37:35 -0600

    fixed some tests

--- test/PriorityQueueTest.js
+++ test/PriorityQueueTest.js
@@ -1,39 +1,39 @@
-const DataStructures = require( ".." );
+const DataStructures = require("..");
 const COUNT = 100;
 
 const {
-        ArrayQueue,
-        Queue,
-    } =  DataStructures.Structures.Queues;
+    LinkedList,
+    ArrayList,
+} = DataStructures.Structures.Queues;
 const {
-        ArrayPriorityQueue,
-        HeapPriorityQueue,
-        MinHeapPriorityQueue,
-        MaxHeapPriorityQueue,
-        PriorityQueue,
-        TreePriorityQueue,
-    } =  DataStructures.Structures.PriorityQueues;
+    BinaryHeap,
+    BinarySearchTree,
+    MinBinaryHeap,
+    MaxBinaryHeap,
+    OrderedArrayList,
+    RedBlackTree,
+} = DataStructures.Structures.PriorityQueues;
 
 const numbers = [];
-for( let i=0 ; i<COUNT ; i++ ) {
-    numbers.push( COUNT*Math.random()|0 );
+for (let i = 0; i < COUNT; i++) {
+    numbers.push(COUNT * Math.random() | 0);
 }
 
-const queue = new MaxHeapPriorityQueue();
+const queue = new MaxBinaryHeap();
 
 let start;
 let stop;
 start = new Date();
-for( let n of numbers ) {
-    queue.add( n );
-    console.log( queue.toArray() );
+for (let n of numbers) {
+    queue.add(n);
+    console.log(queue.toArray());
 }
 stop = new Date();
 // console.log( "%j", queue.toArray() );
 // queue.clear();
-console.log( stop - start );
+console.log(stop - start);
 
-// const treeQueue = new TreePriorityQueue( {allowDupes:false} );
+// const treeQueue = new BinarySearchTree( {allowDupes:false} );
 // start = new Date();
 // // for( let i=0 ; i<COUNT ; i++ ) {
 // for( let n of numbers ) {
